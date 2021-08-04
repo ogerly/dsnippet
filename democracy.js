794104a8f981fbc9c190a9b6a08579c94e1b4134
ogerly <fridolin@tutanota.com>
2021-08-04 22:32:18 +0200

    step2

--- democracy.js
+++ democracy.js
@@ -1,23 +1,19 @@
 $(document).ready(function(){
     $('.container-text').css('display', 'none'); 
+    $( 'div.haken').css('content','&lsaquo;');
     $('.container-1 .container-text').css('display', 'inline-flex');
-    $('.containerrow div.haken:after').css({
-        'content': '‹',
-        'transform': 'rotate(272deg)'        
-    })
-    $('div.haken').text('‹')
+   
+    console.log("start")
+  
    
     function html(clas){
-        console.log(''+ clas +'.content-open')
+        //console.log(''+ clas +'.content-open')
         var html = $('.rectanglebox.content-open').html();
-        console.log(html)
+      
         $('.rectanglebox.content-open').html('').removeClass('content-open');
-        //$(clas +' .rectanglebox').prepend(html).addClass('content-open');
-        //$('.rectanglebox').css('display', 'inline-flex');
         $( ''+clas +' .rectanglebox').html(html);
-        $( ''+clas +' .rectanglebox').addClass('content-open');
-        console.log(''+clas +'.ri-subtitle-heading div.haken');
-        $( ''+clas +'.ri-subtitle-heading div.haken').css('content','');
+        $( ''+clas +' .rectanglebox').addClass('content-open');        
+        //$( ''+clas +'.ri-subtitle-heading div.haken').css('content','');
     }
     
     $('.container-1 .ri-subtitle-heading').click( function() {
@@ -28,7 +24,7 @@ $(document).ready(function(){
     $('.container-2 .ri-subtitle-heading').click( function() {
         $('.container-text').css('display', 'none'); 
         $('.container-2 .container-text').css('display', 'inline-flex');
-        $('.container-2').prepend($('#rectangleinfo').html());
+        //$('.container-2').prepend($('#rectangleinfo').html());
         html('.container-2');
     })
     $('.container-3 .ri-subtitle-heading').click( function() {
